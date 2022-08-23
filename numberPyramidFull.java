a10fceec6836c336c267454ac1a8802ae3211f97
unknown <aanchalannu02@gmail.com>
2022-08-23 16:25:08 +0530

    newPrograms

---
+++ numberPyramidFull.java
@@ -0,0 +1,20 @@
+public class numberPyramidFull {
+  public static void main(String[] args) {
+    int n=5;
+
+    for(int i=1; i<=n; i++){
+      //Spaces
+      for(int j=1; j<=(n-i); j++){
+        System.out.print(" ");
+      }
+
+      //Numbers
+      for(int j=1; j<=i; j++){
+        System.out.print(i);
+        System.out.print(" ");
+      }
+
+      System.out.println();
+    }
+  }
+}
